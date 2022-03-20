7b1ac60be265875113c9c7204f3a67c4dbb43359
Łukasz Bielenin <Lukasz.Bielenin@microsoft.wsei.edu.pl>
2022-03-20 15:53:12 +0100

    initial commit

---
+++ ProcessorSimulator/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Registries } from './models/registry';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ProcessorSimulator';
+
+  initialRegistries: Registries = this.GetInitialRegistries()
+  actualRegistries: Registries =  this.GetInitialRegistries();
+  
+  private GetInitialRegistries(): Registries {
+    return { AH: "", AL: "", BH: "", BL: "", CH: "", CL: "", DH: "", DL: "" }
+  }
+}
---
+++ ProcessorSimulator/src/app/models/registry.ts
@@ -0,0 +1,10 @@
+export interface Registries {
+    AH: string;
+    BH: string;
+    CH: string;
+    DH: string;
+    AL: string;
+    BL: string;
+    CL: string;
+    DL: string;
+}
